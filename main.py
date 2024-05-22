nlnwa/corpus-build

commit 5dc477f1765828d57f8e3fff96fa4de16c014fd1

feat: tokenize
# Motivation

To be able to create a corpus, the text need to be
tokenized. The tokens replaces full text.

main.py
@@ -5,6 +5,7 @@ from datetime import datetime
 from pathlib import Path
 from typing import Generator
 
+from nb_tokenizer import tokenize
 from psycopg2 import connect
 from psycopg2.extensions import cursor
 from tqdm import tqdm
@@ -183,8 +184,11 @@ def _main() -> None:
                 )
                 for fulltext_metadata in tqdm(fulltext_metadata_collection):
                     fulltext_for_domain_dict[fulltext_metadata.hash] = {
-                        "text": _fetch_fulltext_with_fulltext_hash(
-                            database_cursor, fulltext_metadata.hash
+                        "text": map(
+                            tokenize,
+                            _fetch_fulltext_with_fulltext_hash(
+                                database_cursor, fulltext_metadata.hash
+                            ),
                         ),
                         "timestamp": fulltext_metadata.timestamp,
                         "uri": fulltext_metadata.uri,

requirements.in
@@ -1,3 +1,4 @@
 psycopg2-binary # Should not be used in production, see https://www.psycopg.org/docs/install.html#psycopg-vs-psycopg-binary
 pyyaml
 tqdm
+nb_tokenizer

commit 53ebbd6338d957eaa5616bb12c069cca88309bff

fix: use list not iterator
Commit 5dc477f1765828d57f8e3fff96fa4de16c014fd1
introduced a bug by trying to write an iterator to
a `YAML` file.

This bug is easily fixed by simply turning the
iterator into a list.

This is how the output with the iterator looked
like:

```yaml
text-entry:
  da39a3ee5e6b4b0d3255bfef95601890afd80709:
    text: !!python/object/apply:builtins.map
    - &id001 !!python/name:nb_tokenizer.tokenize ''
    - !!python/object/apply:builtins.iter
      args:
      - []
      state: 0
    timestamp: '20190219'
    uri: https://www.gat.no/
  c4fe03c3ebf4a10a5e7d7f83ba988af51a36da35:
    text: !!python/object/apply:builtins.map
```

main.py
@@ -184,11 +184,13 @@ def _main() -> None:
                 )
                 for fulltext_metadata in tqdm(fulltext_metadata_collection):
                     fulltext_for_domain_dict[fulltext_metadata.hash] = {
-                        "text": map(
-                            tokenize,
-                            _fetch_fulltext_with_fulltext_hash(
-                                database_cursor, fulltext_metadata.hash
-                            ),
+                        "text": list(
+                            map(
+                                tokenize,
+                                _fetch_fulltext_with_fulltext_hash(
+                                    database_cursor, fulltext_metadata.hash
+                                ),
+                            )
                         ),
                         "timestamp": fulltext_metadata.timestamp,
                         "uri": fulltext_metadata.uri,